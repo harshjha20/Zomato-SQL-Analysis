2e307bf7984a4ca9edc5f1cd1b72cd295f0638b9
Harsh jha <hjha1925@gmail.com>
2026-06-29 17:29:59 +0530

    Create 01_database_setup.sql

---
+++ SQL/01_database_setup.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE Zomato;
+USE Zomato;
+
+CREATE TABLE zomato(
+    name VARCHAR(150),
+    online_order VARCHAR(10),
+    book_table VARCHAR(10),
+    rate VARCHAR(20),
+    votes INT,
+    approx_cost_for_two VARCHAR(20),
+    listed_type VARCHAR(50)
+);
